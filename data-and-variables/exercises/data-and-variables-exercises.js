4f848d24111f1d32d0dc5f59d2990f9ae3e22128
Elizabeth Levinson <lizarose266@gmail.com>
2024-03-25 15:34:53 -0500

    exercise

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,30 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+
+
+let shuttleName = 'Determination';
+let shuttleSpeed = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 384400;
+let milesPerKilometer = 0.621;
+
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeed);
+console.log(typeof distanceToMarsKm);
+console.log(typeof distanceToMoonKm);
+console.log(typeof milesPerKilometer);
+
+let milesToMars = distanceToMarsKm * milesPerKilometer;
+let hoursToMars = milesToMars / shuttleSpeed;
+let daysToMars = hoursToMars / 24;
+
+let milesToMoon = distanceToMoonKm * milesPerKilometer;
+let hoursToMoon = milesToMoon / shuttleSpeed;
+let daysToMoon = hoursToMoon / 24;
+
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars.");
+
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");
+
