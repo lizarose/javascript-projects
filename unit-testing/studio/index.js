ff946b70188305fa050d3a3ea9e004606afa2239
Elizabeth Levinson <lizarose266@gmail.com>
2024-04-11 15:23:06 -0500

    Unit Testing Studio Completed

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,52 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: "100",
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(num) {
+    let phrase;
+
+    if(num % 2 !== 0 && num % 3 !== 0 && num % 5 !== 0) {
+        phrase = "Rutabagas! That doesn't work.";
+        return phrase;
+
+    } else if (num % 2 === 0 && num % 3 === 0 && num % 5 ===0) {
+        phrase = "LaunchCode Rocks!";
+        return phrase;
+
+    } else if (num % 2 === 0 && num % 5 === 0) {
+        phrase = "Launch Rocks!";
+        return phrase;
+
+    } else if (num % 3 === 0 && num % 5 === 0) {
+        phrase = "Code Rocks!";
+        return phrase;
+
+    } else if (num % 2 === 0 && num % 3 === 0) {
+        phrase = "LaunchCode!";
+        return phrase;
+
+    } else if (num % 5 === 0) {
+        phrase = "Rocks!";
+        return phrase;
+
+    } else if (num % 3 === 0) {
+        phrase = "Code!";
+        return phrase;
+
+    } else if (num % 2 === 0) {
+        phrase = "Launch!";
+        return phrase;
+
+    } else {
+        phrase = "Not Acceptable. Try Again."
+        return phrase;
+    }
+  }
+
 }
+console.log(launchcode.launchOutput())
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,48 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+    // Write your unit tests here!
+  test("should be true if it is nonprofit", function () {
+    expect(launchcode.organization).toEqual("nonprofit")
+  });
+  test("should be true if it is Jeff", function () {
+    expect(launchcode.executiveDirector).toEqual("Jeff")
+  });
+  test("should be true if it is 100", function () {
+    expect(launchcode.percentageCoolEmployees).toEqual("100")
+  });
+  test('should be true if it is ["Web Development", "Data Analysis", "Liftoff"]', function () {
+    expect(launchcode.programsOffered).toContain("Web Development");
+    expect(launchcode.programsOffered).toContain("Data Analysis");
+    expect(launchcode.programsOffered).toContain("Liftoff");
+    expect(launchcode.programsOffered.length).toEqual(3);
+  });
+  test("When passed a number that is ONLY divisible by 2", function () {
+    expect(launchcode.launchOutput(4)).toEqual("Launch!")
+  });
+  test("When passed a number that is ONLY divisible by 3", function () {
+    expect(launchcode.launchOutput(9)).toEqual("Code!")
+  });
+  test("When passed a number that is ONLY divisible by 5", function () {
+    expect(launchcode.launchOutput(25)).toEqual("Rocks!")
+  });
+  test("When passed a number that is divisible by 2 AND 3", function () {
+    expect(launchcode.launchOutput(6)).toEqual("LaunchCode!")
+  });
+  test("When passed a number that is divisible by 3 AND 5", function () {
+    expect(launchcode.launchOutput(15)).toEqual("Code Rocks!")
+  });
+  test("When passed a number that is divisible by 2 AND 5", function () {
+    expect(launchcode.launchOutput(10)).toEqual("Launch Rocks!")
+  });
+  test("When passed a number that is divisible by 2, 3, AND 5", function () {
+    expect(launchcode.launchOutput(30)).toEqual("LaunchCode Rocks!")
+  });
+  test("When passed a number that is NOT divisible by 2, 3, or 5", function () {
+    expect(launchcode.launchOutput()).toEqual("Rutabagas! That doesn't work.")
+  });
 
-  // Write your unit tests here!
-  
+
+
 });
+
